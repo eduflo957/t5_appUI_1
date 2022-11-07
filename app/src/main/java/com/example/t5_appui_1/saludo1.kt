aca81807d744593a0a081487c5d4c768f64a3a0d
AlumnoM <eduardoflores.9595@gmail.com>
2022-11-07 11:56:34 +0100

    clase 07.11.22: intento onclick hecho, falta documentarlo

---
+++ app/src/main/java/com/example/t5_appui_1/saludo1.kt
@@ -0,0 +1,11 @@
+package com.example.t5_appui_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class saludo1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo1)
+    }
+}
